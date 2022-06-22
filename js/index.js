f615ac6825e228bd7ea49b4c7ae8806052de0356
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-06-22 11:50:46 +0300

    yhu6yhj6uy

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 export function getTitleElement (){
  const titleElem = document.querySelector('title');
  console.dir(titleElem)
- 
+ return
 }
 
 
 export function getInputElement (){
   const inputElem = document.querySelector('input');
   console.dir(inputElem)
-  
+  return
 }
