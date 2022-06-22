891e7bbcb7300c30912d13a600931d41ef9c0991
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-06-22 18:45:33 +0300

    kiuik7u

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ export function getTitle(){
 getTitle()
 
 export function getDescription() {
-  const innerText = document.querySelector('.about').innerText
-  console.log(innerText)
+  const innerText = document.querySelector('.about').innerText;
+  console.log(innerText);
   return innerText
 }
  getDescription()
 
  export function getPlans(){
    const plansText = document.querySelector('.plans').innerHTML;
-   console.log(plansText)
+   console.log(plansText);
    return plansText
  }
  getPlans()
 
  export function getGoal(){
-   const goalText = document.querySelector('.goal').outerHTML
-   console.log(goalText)
+   const goalText = document.querySelector('.goal').outerHTML;
+   console.log(goalText);
    return goalText
  }
  getGoal()
