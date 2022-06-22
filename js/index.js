a2813214172d72e086ea7e466ed14c74c0253d21
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-06-22 13:34:29 +0300

    лшбдшб

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 
-export function getItemsList(){
+ function getItemsList(){
   const elementsList = document.querySelectorAll('.technology')
   console.dir(elementsList)
-  return
+  
 }
-getItemsList()
 
-export function getItemsArray(){
+
+ function getItemsArray(){
   const elementsArray = document.querySelectorAll('.tool');
   console.dir(Array.from(elementsArray))
 }
-getItemsArray()
