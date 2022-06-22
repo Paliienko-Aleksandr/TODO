5a3fec2732ff49f7b2c47f5e75daed2330dcbba6
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-06-22 16:07:32 +0300

    yhnuhy6nyj

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-  export function getItemsList(){
-  const elementsList = document.querySelectorAll('.technology')
-  console.dir(elementsList)
-  return elementsList
+export function getTitleElement(){
+  const titleElem = document.querySelector('.title')
+  console.dir(titleElem)
+  return titleElem
 }
-getItemsList()
+getTitleElement()
 
-  export function getItemsArray(){
-  const elementsArray = document.querySelectorAll('.tool');
-  console.dir(Array.from(elementsArray))
-  return elementsArray
+export function getInputElement(){
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem)
+  return inputElem
 }
-getItemsArray()
+getInputElement()
