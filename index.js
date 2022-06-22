64cc60dbc514cf96c59633233286694c9729c2ff
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-06-22 18:37:47 +0300

    yjikk,8i

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-function getTitle(){
+export function getTitle(){
   const titleText = document.querySelector('.title').textContent;
   console.log(titleText)
   return titleText
 }
 getTitle()
 
-function getDescription() {
+export function getDescription() {
   const innerText = document.querySelector('.about').innerText
   console.log(innerText)
   return innerText
 }
  getDescription()
 
- function getPlans(){
+ export function getPlans(){
    const plansText = document.querySelector('.plans').innerHTML;
    console.log(plansText)
    return plansText
  }
  getPlans()
 
- function getGoal(){
+ export function getGoal(){
    const goalText = document.querySelector('.goal').outerHTML
    console.log(goalText)
    return goalText
