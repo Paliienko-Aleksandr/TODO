eee78c6402f2f6ecc5bafc7c01da8f54ceb55564
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-07-01 15:57:11 +0300

    7гр6нгр

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const tasks = [
    { text: 'Buy milk', done: false , id: 1},
    { text: 'Pick up Tom from airport', done: false, id: 2},
-   { text: 'Visit party', done: false, id: 3 },
-   { text: 'Visit doctor', done: true, id: 4 },
-   { text: 'Buy meat', done: true, id: 5 },
+   { text: 'Visit party', done: false, id: 3},
+   { text: 'Visit doctor', done: true, id: 4},
+   { text: 'Buy meat', done: true, id: 5},
  ];
  
  const listElem = document.querySelector('.list');
@@ -13,12 +13,12 @@ const tasks = [
   listElem.innerHTML = ''
    const tasksElems = tasksList
      .sort((a, b) => a.done - b.done)
-     .map(({ text, done}) => {
+     .map(({ text, done, id}) => {
        const listItemElem = document.createElement('li');
        listItemElem.classList.add('list__item');
        const checkbox = document.createElement('input');
        checkbox.setAttribute('type', 'checkbox');
-       checkbox.setAttribute('data', 'id = 1');
+       checkbox.setAttribute('data-id', id);
        checkbox.checked = done;
        checkbox.classList.add('list__item-checkbox');
        if (done) {
