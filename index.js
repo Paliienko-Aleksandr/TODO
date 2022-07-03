b8b260846a80624d3deb9eacafe29b5f427e0739
Paliienko-Aleksandr <89596420+Paliienko-Aleksandr@users.noreply.github.com>
2022-07-03 15:41:03 +0300

    fgergerf

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const tasks = [
    { text: 'Buy milk', done: false , id: Math.random(1)*10000/10},
    { text: 'Pick up Tom from airport', done: false, id: Math.random(1)*10000/10},
-   { text: 'Visit party', done: false, id: Math.random(1)*10000/10},
+   { text: 'Visit party', done: true, id: Math.random(1)*10000/10},
    { text: 'Visit doctor', done: false, id: Math.random(1)*10000/10},
-   { text: 'Buy meat', done: false, id: Math.random(1)*10000/10},
+   { text: 'Buy meat', done: true, id: Math.random(1)*10000/10},
  ];
  
  const listElem = document.querySelector('.list');
@@ -25,49 +25,39 @@ const tasks = [
          listItemElem.classList.add('list__item_done');
        }
        listItemElem.append(checkbox, text);
- 
-      
        return listItemElem;
-
      });
  
    listElem.append(...tasksElems);
 
-   
    const buttonElem = document.querySelector('.create-task-btn');
    const inputElem = document.querySelector('.task-input')
   
-   
    const createNewData = () =>{
      if (inputElem.value === ''){
        return
      }
-
     let obj = {
       text: `${inputElem.value}`,
       done: false,
       id: Math.random(1)*10000/10,
     }
-    
     tasksList.push(obj)
     renderTasks(tasks)
     }
     buttonElem.addEventListener('click', createNewData);
 
- 
-  
   const checkboxChangeStatus = (event) =>{
     tasksList.map((item) =>{
      if( item.id === +(event.target.dataset.id)){
       item.done = true
      }
-     renderTasks(tasks)
+     
     })
-    
   }
+  
     const checkboxElem = Array.from(document.querySelectorAll('.list__item-checkbox'))
     checkboxElem.map((item) => {
-      
       item.addEventListener('click', checkboxChangeStatus)
     })
 };
